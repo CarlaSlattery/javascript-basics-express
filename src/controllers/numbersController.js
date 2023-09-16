6523f9ecedafa7d15a4b668ab1609d3717a5dfed
CarlaSlattery <cslatterydev@gmail.com>
2023-09-16 17:42:18 +0100

    TEST - Passes all tests for divideNums function

--- src/controllers/numbersController.js
+++ src/controllers/numbersController.js
@@ -32,9 +32,18 @@ const multiplyNums = (req, res) => {
 };
 
 const divideNums = (req, res) => {
-  const num1 = parseInt(req.body.a, 10);
-  const num2 = parseInt(req.body.b, 10);
-  return res.status(200).json({ result: divide(num1, num2) });
+  const num1 = req.body.a;
+  const num2 = req.body.b;
+
+  if (num2 === 0) {
+    res.status(400).json({ error: 'Unable to divide by 0.' });
+  } else if ((!num1 || !num2) && num1 !== 0) {
+    res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
+  } else if (Number.isNaN(parseInt(num1, 10)) || Number.isNaN(parseInt(num2, 10))) {
+    res.status(400).json({ error: 'Parameters "a" and "b" must be valid numbers.' });
+  } else {
+    res.status(200).json({ result: divide(num1, num2) });
+  }
 };
 
 const remainderNums = (req, res) => {
